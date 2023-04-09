35e6c1189fd7935923a625442e9bebb4d925b05c
Gandolh <29356778+gandolh@users.noreply.github.com>
2023-04-10 00:04:12 +0300

    last try

---
+++ gatsby-config.js
@@ -0,0 +1,18 @@
+module.exports = {
+    pathPrefix: '/presentation-deck',
+    plugins: [
+      'gatsby-plugin-catch-links',
+      {
+        resolve: 'gatsby-theme-mdx-deck',
+        options: {
+          basePath: '/presentation-deck',
+        },
+    },
+    {
+        resolve: "gatsby-plugin-compile-es6-packages",
+        options: {
+        modules: ["@mdx-deck/gatsby-plugin"]
+        }
+    }
+    ],
+  }
