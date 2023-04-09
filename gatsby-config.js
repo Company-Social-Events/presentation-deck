e58d5b9295a84a951c0e7eb256b651e757a11d37
Gandolh <29356778+gandolh@users.noreply.github.com>
2023-04-10 00:51:31 +0300

    alors on danse

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,18 +1,18 @@
 module.exports = {
-    pathPrefix: '',
-    plugins: [
-      'gatsby-plugin-catch-links',
-      {
-        resolve: 'gatsby-theme-mdx-deck',
-        options: {
-          basePath: '/slides',
-        },
-    },
+  pathPrefix: '/presentation-deck',
+  plugins: [
+    'gatsby-plugin-catch-links',
     {
-        resolve: "gatsby-plugin-compile-es6-packages",
-        options: {
-        modules: ["@mdx-deck/gatsby-plugin"]
-        }
-    }
-    ],
+      resolve: 'gatsby-theme-mdx-deck',
+      options: {
+        basePath: '/slides',
+      },
+  },
+  {
+      resolve: "gatsby-plugin-compile-es6-packages",
+      options: {
+      modules: ["@mdx-deck/gatsby-plugin"]
+      }
   }
+  ],
+}
